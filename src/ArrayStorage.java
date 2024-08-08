2e3a722c493a00aefe110e078ca066fde1a55642
yanarsaeev <mackardyback@internet.ru>
2024-08-08 19:22:33 +0300

    developed methods: clear(), save(), get(), delete(), getAll(), size()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,59 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) continue;
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        size++;
+        storage[size - 1] = r;
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume != null && uuid.equals(resume.toString())) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                storage[i] = null;
+                size--;
+                break;
+            }
+        }
+
+        for (int i = 1; i < storage.length; i++) {
+            if (storage[i - 1] == null && storage[i] != null) {
+                storage[i - 1] = storage[i];
+                storage[i] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
