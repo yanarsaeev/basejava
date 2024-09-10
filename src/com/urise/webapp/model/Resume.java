3b28d95b72d71e43ad96c86108857d1337f0511b
mackardyback <mackardyback@internet.ru>
2024-09-10 11:58:07 +0300

    added getter/setter

---
+++ src/com/urise/webapp/model/Resume.java
@@ -0,0 +1,18 @@
+package com.urise.webapp.model;
+
+public class Resume {
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}
