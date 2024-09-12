8a80bfd11d48cc6dbd6c1df849ac7fd99b5bf08a
mackardyback <mackardyback@internet.ru>
2024-09-12 19:19:54 +0300

    fixed error

--- src/com/urise/webapp/storage/MainArray.java
+++ src/com/urise/webapp/storage/MainArray.java
@@ -11,7 +11,7 @@ public class MainArray {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Resume r = null;
+        Resume r;
         while (true) {
             System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
