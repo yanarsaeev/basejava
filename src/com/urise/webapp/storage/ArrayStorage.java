a51f58d3c20d2f9f95f340a19313d0947a8a59f2
mackardyback <mackardyback@internet.ru>
2024-09-15 22:44:08 +0300

    implements Storage interface

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -4,10 +4,10 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-public class ArrayStorage {
+public class ArrayStorage implements Storage{
     private static final int STORAGE_LIMIT = 10000;
     private final Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size;
+    private int size = 0;
 
     public void clear() {
         Arrays.fill(storage, null);
