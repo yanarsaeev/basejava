f91143958b68f0276321dfbfc06380512fdb4864
mackardyback <mackardyback@internet.ru>
2024-09-18 00:40:03 +0300

    add abstract class

---
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,24 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10000;
+    protected final Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            return null;
+        }
+
+        return storage[index];
+    }
+
+    protected abstract int getIndex(String uuid);
+}
