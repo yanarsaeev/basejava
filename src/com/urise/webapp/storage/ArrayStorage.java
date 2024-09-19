c1c105c19346109f884006a46d942a9e9c59ed3f
mackardyback <mackardyback@internet.ru>
2024-09-19 22:42:42 +0300

    implement abstract methods

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,50 +1,12 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.model.Resume;
-
-import java.util.Arrays;
-
 public class ArrayStorage extends AbstractArrayStorage {
-    public void clear() {
-        Arrays.fill(storage, null);
-        size = 0;
+    @Override
+    protected void fillEmptyCell(int index) {
+        storage[index] = storage[size - 1];
     }
 
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index == -1) {
-            System.out.println("Резюме нет в БД");
-        } else {
-            storage[index] = r;
-        }
-    }
-
-    public void save(Resume r) {
-        if (size > STORAGE_LIMIT) {
-            System.out.println("Нет места в БД");
-        } else if (getIndex(r.getUuid()) != -1) {
-            System.out.println("Резюме уже есть в БД");
-        } else {
-            storage[size] = r;
-            size++;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Резюме " + uuid + " нет в БД");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
+    @Override
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
