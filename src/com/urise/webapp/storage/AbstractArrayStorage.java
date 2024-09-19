ab3f6382849cf983cecbd0f38231837f83e9ee4f
mackardyback <mackardyback@internet.ru>
2024-09-19 22:42:12 +0300

    add abstract method fillEmptyCell()

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -2,8 +2,11 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage {
     protected static final int STORAGE_LIMIT = 10000;
+
     protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
@@ -20,5 +23,46 @@ public abstract class AbstractArrayStorage implements Storage {
         return storage[index];
     }
 
+    public void clear() {
+        Arrays.fill(storage, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Резюме нет в БД");
+        } else {
+            storage[index] = r;
+        }
+    }
+
+    public void save(Resume r) {
+        if (size > STORAGE_LIMIT) {
+            System.out.println("Нет места в БД");
+        } else if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Резюме уже есть в БД");
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Резюме " + uuid + " нет в БД");
+        } else {
+            fillEmptyCell(index);
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
     protected abstract int getIndex(String uuid);
+    protected abstract void fillEmptyCell(int index);
 }
