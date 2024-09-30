ec153ecde935e13f4069c3c7b9bfd1b3102c633e
yanarsaeev <mackardyback@internet.ru>
2024-09-30 21:58:24 +0300

    invoke r.toString via reflection

---
+++ src/com/urise/webapp/MainReflection.java
@@ -0,0 +1,24 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, ClassNotFoundException,
+            NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new uuid");
+        // TODO : invoke r.toString via reflection
+        Class<?> clazz = Class.forName("com.urise.webapp.model.Resume");
+        Method method = clazz.getMethod("toString");
+        System.out.println("toString(): " + method.invoke(r));
+        System.out.println(r);
+    }
+}
