8ba5bba8fb1597d29a5e10f73c9b9b09c4ef1d8b
mackardyback <mackardyback@internet.ru>
2024-10-02 22:11:32 +0300

    add constructor

--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -1,7 +1,7 @@
 package com.urise.webapp.storage;
 
-import static org.junit.Assert.*;
-
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
-
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
 }
