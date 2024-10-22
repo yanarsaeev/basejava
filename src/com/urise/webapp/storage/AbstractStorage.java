a8fb86e13a09f01ac08d68fe0fc6ae8acb281040
yanarsaeev <mackardyback@internet.ru>
2024-10-23 02:00:38 +0300

    add methods

---
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -0,0 +1,37 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.ExistStorageException;
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.model.Resume;
+
+
+public abstract class AbstractStorage<T> implements Storage {
+    protected abstract void saver(T key, Resume r);
+    protected abstract void deleter(String uuid, T key);
+    protected abstract Resume getter(String uuid, T key);
+    protected abstract boolean isRepeat(String uuid, T key);
+
+    public Resume get(String uuid) {
+        if (!isRepeat(uuid, (T) uuid)) {
+            throw new NotExistStorageException(uuid);
+        }
+        return getter(uuid, (T) uuid);
+    }
+
+    public void update(Resume r) {
+    }
+
+    public void save(Resume r) {
+        if (isRepeat(r.getUuid(), (T) r.getUuid())) {
+            throw new ExistStorageException(r.getUuid());
+        }
+        saver((T) r.getUuid(), r);
+    }
+
+    public void delete(String uuid) {
+        if (!isRepeat(uuid, (T) uuid)) {
+            throw new NotExistStorageException(uuid);
+        }
+        deleter(uuid, (T) uuid);
+    }
+}
