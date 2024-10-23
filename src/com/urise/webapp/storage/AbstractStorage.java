d631c34bafdc7bf17411a6eacc3960262376f870
yanarsaeev <mackardyback@internet.ru>
2024-10-24 00:07:28 +0300

    rename methods

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,37 +6,37 @@ import com.urise.webapp.model.Resume;
 
 
 public abstract class AbstractStorage<T> implements Storage {
-    protected abstract void saver(T key, Resume r);
-    protected abstract void deleter(String uuid, T key);
-    protected abstract Resume getter(String uuid, T key);
-    protected abstract void updater(T key, Resume r);
+    protected abstract void doSave(T key, Resume r);
+    protected abstract void doDelete(String uuid, T key);
+    protected abstract Resume doGet(String uuid, T key);
+    protected abstract void doUpdate(T key, Resume r);
     protected abstract boolean isRepeat(String uuid, T key);
 
     public Resume get(String uuid) {
         if (!isRepeat(uuid, (T) uuid)) {
             throw new NotExistStorageException(uuid);
         }
-        return getter(uuid, (T) uuid);
+        return doGet(uuid, (T) uuid);
     }
 
     public void update(Resume r) {
         if (!isRepeat(r.getUuid(), (T) r.getUuid())) {
             throw new NotExistStorageException(r.getUuid());
         }
-        updater((T) r.getUuid(), r);
+        doUpdate((T) r.getUuid(), r);
     }
 
     public void save(Resume r) {
         if (isRepeat(r.getUuid(), (T) r.getUuid())) {
             throw new ExistStorageException(r.getUuid());
         }
-        saver((T) r.getUuid(), r);
+        doSave((T) r.getUuid(), r);
     }
 
     public void delete(String uuid) {
         if (!isRepeat(uuid, (T) uuid)) {
             throw new NotExistStorageException(uuid);
         }
-        deleter(uuid, (T) uuid);
+        doDelete(uuid, (T) uuid);
     }
 }
