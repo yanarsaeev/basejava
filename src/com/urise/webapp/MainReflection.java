e45edf70ae1c51744cde104cc690ea952a966a2e
yanarsaeev <mackardyback@internet.ru>
2024-11-08 13:16:21 +0300

    pass parameter

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -9,7 +9,7 @@ import java.lang.reflect.Method;
 public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException, ClassNotFoundException,
             NoSuchMethodException, InvocationTargetException {
-        Resume r = new Resume();
+        Resume r = new Resume("ali");
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
