787726b5029318cb8d6e0edb08a84762236898fd
yanarsaeev <mackardyback@internet.ru>
2024-11-08 13:14:51 +0300

    refactor getAll() -> getAllSorted()

--- src/com/urise/webapp/storage/Storage.java
+++ src/com/urise/webapp/storage/Storage.java
@@ -2,12 +2,14 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.List;
+
 public interface Storage {
     void clear();
     void update(Resume r);
     void save(Resume r);
     Resume get(String uuid);
     void delete(String uuid);
-    Resume[] getAll();
+    List<Resume> getAllSorted();
     int size();
 }
