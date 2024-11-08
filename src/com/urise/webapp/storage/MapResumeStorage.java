6585781d9e71ea564d2e16a691567105e752cf5a
yanarsaeev <mackardyback@internet.ru>
2024-11-08 13:13:01 +0300

    implement getSearchKey(), update getAll()

--- src/com/urise/webapp/storage/MapResumeStorage.java
+++ src/com/urise/webapp/storage/MapResumeStorage.java
@@ -1,64 +1,58 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.exception.ExistStorageException;
-import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MapResumeStorage extends AbstractStorage<Resume> {
-    Map<String, Resume> map = new HashMap<>();
+    private final Map<String, Resume> storage = new HashMap<>();
 
     @Override
     protected void doSave(Resume searchKey, Resume resume) {
-        if (isExisting(searchKey)) {
-            throw new ExistStorageException(searchKey.getUuid());
-        }
-        map.put(searchKey.getUuid(), searchKey);
+        storage.put(searchKey.getUuid(), resume);
     }
 
     @Override
-    protected void doDelete(Resume searchKey, Resume resume) {
-        if (!isExisting(searchKey)) {
-            throw new NotExistStorageException(searchKey.getUuid());
-        }
-        map.remove(searchKey.getUuid());
+    protected void doDelete(Resume searchKey) {
+        storage.remove(searchKey.getUuid());
     }
 
     @Override
-    protected Resume doGet(Resume searchKey, Resume resume) {
-        if (!isExisting(searchKey)) {
-            throw new NotExistStorageException(searchKey.getUuid());
-        }
-        return map.get(searchKey.getUuid());
+    protected Resume doGet(Resume searchKey) {
+        return searchKey;
     }
 
     @Override
     protected void doUpdate(Resume searchKey, Resume resume) {
-        if (!isExisting(searchKey)) {
-            throw new NotExistStorageException(searchKey.getUuid());
-        }
-        map.put(searchKey.getUuid(), searchKey);
+        storage.put(searchKey.getUuid(), searchKey);
     }
 
     @Override
     protected boolean isExisting(Resume searchKey) {
-        return map.containsValue(searchKey);
+        return searchKey != null;
+    }
+
+    @Override
+    protected Resume getSearchKey(String uuid) {
+        return storage.get(uuid);
     }
 
     @Override
     public void clear() {
-        map.clear();
+        storage.clear();
     }
 
     @Override
-    public Resume[] getAll() {
-        return new Resume[0];
+    public List<Resume> getAll() {
+        return new ArrayList<>(storage.values());
     }
+
 
     @Override
     public int size() {
-        return map.size();
+        return storage.size();
     }
 }
