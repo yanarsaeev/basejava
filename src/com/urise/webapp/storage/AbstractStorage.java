eede5ea12ab5d161e9012b55f531771c0bd3a845
yanarsaeev <mackardyback@internet.ru>
2024-11-08 20:46:50 +0300

    add logger

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,9 +6,11 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 
 public abstract class AbstractStorage<T> implements Storage {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
     protected abstract void doSave(T searchKey, Resume r);
     protected abstract void doDelete(T searchKey);
     protected abstract Resume doGet(T searchKey);
@@ -21,6 +23,7 @@ public abstract class AbstractStorage<T> implements Storage {
     public T getExistingSearchKey(String uuid) {
         T searchKey = getSearchKey(uuid);
         if (!isExisting(searchKey)) {
+            LOG.warning("Резюме " + uuid + " нет в БД");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
@@ -29,33 +32,39 @@ public abstract class AbstractStorage<T> implements Storage {
     public T getNotExistingSearchKey(String uuid) {
         T searchKey = getSearchKey(uuid);
         if (isExisting(searchKey)) {
+            LOG.warning("Резюме " + uuid + " уже есть в БД");
             throw new ExistStorageException(uuid);
         }
         return searchKey;
     }
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         T key = getExistingSearchKey(uuid);
         return doGet(key);
     }
 
     public void update(Resume r) {
+        LOG.info("Update " + r);
         T key = getExistingSearchKey(r.getUuid());
         doUpdate(key, r);
     }
 
     public void save(Resume r) {
+        LOG.info("Save " + r);
         T key = getNotExistingSearchKey(r.getUuid());
         doSave(key, r);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         T key = getExistingSearchKey(uuid);
         doDelete(key);
     }
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
         List<Resume> list = getAll();
         list.sort(RESUME_COMPARATOR);
