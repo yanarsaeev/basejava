3937cb2f1112b9c0c9fdcb69c3a372cd7300ac28
yanarsaeev <mackardyback@internet.ru>
2024-11-12 18:12:40 +0300

    add Company section

---
+++ src/com/urise/webapp/model/CompanySection.java
@@ -0,0 +1,37 @@
+package com.urise.webapp.model;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class CompanySection extends Section {
+    private final List<Company> companies;
+
+    CompanySection() {
+        this.companies = new LinkedList<>();
+    }
+
+    public void addCompany(Company company) {
+        companies.add(company);
+    }
+
+    @Override
+    public String toString() {
+        return "CompanySection{" +
+                "companies=" + companies +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CompanySection that = (CompanySection) o;
+        return Objects.equals(companies, that.companies);
+    }
+
+    @Override
+    public int hashCode() {
+        return companies.hashCode();
+    }
+}
