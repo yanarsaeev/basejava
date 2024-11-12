cf0fc062067e54db077acab2983bbd96dc0002db
yanarsaeev <mackardyback@internet.ru>
2024-11-12 18:12:32 +0300

    add Company

---
+++ src/com/urise/webapp/model/Company.java
@@ -0,0 +1,43 @@
+package com.urise.webapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Company {
+    private final String name;
+    private final String website;
+    private final List<Period> periods;
+
+    Company(String name, String website) {
+        this.name = name;
+        this.website = website;
+        this.periods = new ArrayList<>();
+    }
+
+    public void addPeriod(Period period) {
+        periods.add(period);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name +
+                "\nWebsite: " + this.website +
+                "\nPeriods: " + this.periods;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return Objects.equals(name, company.name) &&
+                Objects.equals(website, company.website) &&
+                Objects.equals(periods, company.periods);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, website, periods);
+    }
+}
