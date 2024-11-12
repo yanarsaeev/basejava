5ceeda82f478c70d152e171f0d860bdd8b0f60cf
yanarsaeev <mackardyback@internet.ru>
2024-11-12 18:13:29 +0300

    add EnumMap contacts, sections

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -1,10 +1,22 @@
 package com.urise.webapp.model;
 
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.UUID;
 
 public final class Resume implements Comparable<Resume> {
     private final String fullName;
     private final String uuid;
+    private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
+    private final Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
+
+    public void addSection(SectionType type, Section section) {
+        sections.put(type, section);
+    }
+
+    public Map<SectionType, Section> getSections() {
+        return sections;
+    }
 
     public Resume(String fullName) {
         this.fullName = fullName;
@@ -14,6 +26,14 @@ public final class Resume implements Comparable<Resume> {
     public Resume(String uuid, String fullName) {
         this.uuid = uuid;
         this.fullName = fullName;
+    }
+
+    public String getContactType(ContactType type) {
+        return contacts.get(type);
+    }
+
+    public Section getSectionType(SectionType type) {
+        return sections.get(type);
     }
 
     public String getUuid() {
