7ebf0e2c587807a16741fd3aee087ee6f365b29b
yanarsaeev <mackardyback@internet.ru>
2024-11-12 18:13:15 +0300

    implement Period

---
+++ src/com/urise/webapp/model/Period.java
@@ -0,0 +1,42 @@
+package com.urise.webapp.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Period {
+    private final LocalDate start;
+    private final LocalDate end;
+    private final String title;
+    private final String description;
+
+    Period(LocalDate start, LocalDate end, String title, String description) {
+        this.start = start;
+        this.end = end;
+        this.title = title;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Period{" +
+                "start=" + start +
+                ", end=" + end +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Period period = (Period) o;
+        return Objects.equals(start, period.start) && Objects.equals(end, period.end) &&
+                Objects.equals(title, period.title) && Objects.equals(description, period.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end, title, description);
+    }
+}
