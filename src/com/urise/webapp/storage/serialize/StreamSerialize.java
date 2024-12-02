fa40e19559de925986ab46a13680febc7426ddcc
yanarsaeev <mackardyback@internet.ru>
2024-12-02 23:29:59 +0300

    implement StreamSerialize

---
+++ src/com/urise/webapp/storage/serialize/StreamSerialize.java
@@ -0,0 +1,12 @@
+package com.urise.webapp.storage.serialize;
+
+import com.urise.webapp.model.Resume;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface StreamSerialize {
+    void doWrite(Resume r, OutputStream os) throws IOException;
+    Resume doRead(InputStream is) throws IOException;
+}
