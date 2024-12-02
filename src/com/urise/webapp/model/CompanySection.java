a91295655f083576dd2b0d86ad88b5f385cd683a
yanarsaeev <mackardyback@internet.ru>
2024-12-02 23:31:45 +0300

    add serialVersionUID

--- src/com/urise/webapp/model/CompanySection.java
+++ src/com/urise/webapp/model/CompanySection.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Objects;
 
 public class CompanySection extends Section {
+    private static final long serialVersionUID = 1L;
+
     private final List<Company> companies;
 
     public CompanySection() {
