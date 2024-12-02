654735e1f938bc9c29f379967111c7b255a6f582
yanarsaeev <mackardyback@internet.ru>
2024-12-02 23:29:36 +0300

    add PathStorage

---
+++ src/com/urise/webapp/storage/PathStorage.java
@@ -0,0 +1,109 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.serialize.StreamSerialize;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class PathStorage extends AbstractStorage<Path> {
+    private final Path directory;
+
+    StreamSerialize streamSerialize;
+
+    protected PathStorage(String dir, StreamSerialize streamSerialize) {
+        directory = Paths.get(dir);
+        this.streamSerialize = streamSerialize;
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!Files.isDirectory(directory) || !Files.isWritable(directory)) {
+            throw new IllegalArgumentException(dir + " is not directory or is not writable");
+        }
+    }
+
+    @Override
+    protected void doSave(Path path, Resume r) {
+        try {
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new StorageException("Path save error" + path.getFileName(), r.getUuid(), e);
+        }
+    }
+
+    @Override
+    protected void doDelete(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", path.getFileName().toString());
+        }
+    }
+
+    @Override
+    protected Resume doGet(Path path) {
+        try {
+            return streamSerialize.doRead(new BufferedInputStream(Files.newInputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("Path get error", path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected void doUpdate(Path path, Resume r) {
+        try {
+            streamSerialize.doWrite(r, new BufferedOutputStream(Files.newOutputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("Path update error", r.getUuid(), e);
+        }
+    }
+
+    @Override
+    protected boolean isExisting(Path path) {
+        return Files.exists(path);
+    }
+
+    @Override
+    protected Path getSearchKey(String uuid) {
+        return directory.resolve(uuid);
+    }
+
+    @Override
+    protected List<Resume> getAll() {
+        Path[] paths;
+        try {
+            paths = (Path[]) Files.list(directory).toArray();
+        } catch (IOException e) {
+            throw new StorageException("path getAll error", null);
+        }
+
+        List<Resume> list = new ArrayList<>(paths.length);
+        for (Path path : paths) {
+            list.add(doGet(path));
+        }
+        return list;
+    }
+
+    @Override
+    public void clear() {
+        try (Stream<Path> paths = Files.list(directory)) {
+            paths.forEach(this::doDelete);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", null);
+        }
+    }
+
+    @Override
+    public int size() {
+        String[] list = directory.toFile().list();
+        if (list == null) {
+            throw new StorageException("Directory read error", null);
+        }
+        return list.length;
+    }
+}
