5d4f335d0c2144c43b06fff510808cb169019d73
yanarsaeev <mackardyback@internet.ru>
2024-12-10 23:16:45 +0300

    remove

--- test/com/urise/webapp/storage/ObjectStreamPathStorageTest.java
+++
@@ -1,9 +0,0 @@
-package com.urise.webapp.storage;
-
-import com.urise.webapp.storage.serialize.ObjectStreamPathStorage;
-
-public class ObjectStreamPathStorageTest extends AbstractStorageTest {
-    public ObjectStreamPathStorageTest() {
-        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new ObjectStreamPathStorage()));
-    }
-}
