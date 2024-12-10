ae2c2f7bc342be81e7a4f45f8e89c613bba8b77c
yanarsaeev <mackardyback@internet.ru>
2024-12-10 23:18:00 +0300

    updated listSection creater

--- test/com/urise/webapp/storage/ResumeTestData.java
+++ test/com/urise/webapp/storage/ResumeTestData.java
@@ -3,6 +3,8 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.time.LocalDate.*;
 
@@ -14,15 +16,11 @@ public class ResumeTestData {
         TextSection textSection1 = new TextSection("Аналитический склад ума, сильная логика, креативность, " +
                 "инициативность. Пурист кода и архитектуры.");
 
-        ListSection listSection = new ListSection();
-        listSection.addString("Организация команды и успешная реализация Java проектов для сторонних заказчиков");
-        listSection.addString("разработка проектов \"Разработка Web приложения\",\"Java Enterprise\"");
+        ListSection listSection = new ListSection("Организация команды и успешная реализация Java проектов " +
+                "для сторонних заказчиков", "разработка проектов \"Разработка Web приложения\",\"Java Enterprise\"");
 
-        ListSection listSection1 = new ListSection();
-        listSection1.addString("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2");
-        listSection1.addString("XML/XSD/XSLT, SQL, C/C++, Unix shell scripts");
-        listSection1.addString("Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy");
-
+        ListSection listSection1 = new ListSection("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2",
+                "XML/XSD/XSLT, SQL, C/C++, Unix shell scripts", "Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy");
 
         LocalDate start = of(2020, 6, 11);
         LocalDate end = of(2021, 5, 5);
@@ -30,8 +28,11 @@ public class ResumeTestData {
         Company company = new Company("VKontakte", "vk.com");
         company.addPeriod(period);
 
-        CompanySection companySection = new CompanySection();
-        companySection.addCompany(company);
+        List<Company> companyList = new ArrayList<>();
+        companyList.add(company);
+
+        CompanySection companySection = new CompanySection(companyList);
+//        companySection.addCompany(company);
 
         resume.addSection(SectionType.OBJECTIVE, textSection);
         resume.addSection(SectionType.PERSONAL, textSection1);
