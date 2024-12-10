f11be5689964f15eabf33445130d277a0e2367a6
yanarsaeev <mackardyback@internet.ru>
2024-12-10 23:14:03 +0300

    add json adapter

---
+++ src/com/urise/webapp/util/JsonSectionAdapter.java
@@ -0,0 +1,34 @@
+package com.urise.webapp.util;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+
+public class JsonSectionAdapter<T> implements JsonSerializer<T>, JsonDeserializer<T> {
+    private static final String CLASSNAME = "CLASSNAME";
+    private static final String INSTANCE = "INSTANCE";
+
+    @Override
+    public T deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
+            throws JsonParseException {
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        JsonPrimitive prim = (JsonPrimitive) jsonObject.get(CLASSNAME);
+        String className = prim.getAsString();
+
+        try {
+            Class<?> clazz = Class.forName(className);
+            return jsonDeserializationContext.deserialize(jsonObject.get(INSTANCE), clazz);
+        } catch (ClassNotFoundException e) {
+            throw new JsonParseException(e.getMessage());
+        }
+    }
+
+    @Override
+    public JsonElement serialize(T t, Type type, JsonSerializationContext jsonSerializationContext) {
+        JsonObject retValue = new JsonObject();
+        retValue.addProperty(CLASSNAME, t.getClass().getName());
+        JsonElement elem = jsonSerializationContext.serialize(t);
+        retValue.add(INSTANCE, elem);
+        return retValue;
+    }
+}
