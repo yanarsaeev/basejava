ce3c514b483aea1fa516f729e7e848866787f714
yanarsaeev <mackardyback@internet.ru>
2024-12-10 23:15:49 +0300

    add annotation @XmlAccessorType

--- src/com/urise/webapp/model/Company.java
+++ src/com/urise/webapp/model/Company.java
@@ -1,15 +1,20 @@
 package com.urise.webapp.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Company implements Serializable {
     private static final long serialVersionUID = 1L;
-    private final String name;
-    private final String website;
-    private final List<Period> periods;
+    private String name;
+    private String website;
+    private List<Period> periods;
+
+    public Company() {}
 
     public Company(String name, String website) {
         this.name = name;
