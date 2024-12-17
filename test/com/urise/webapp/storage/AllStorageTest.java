0cc31e105f7629468295188b7b8e046da4d86f86
yanarsaeev <mackardyback@internet.ru>
2024-12-17 17:38:58 +0300

    add DataPathStorageTest

--- test/com/urise/webapp/storage/AllStorageTest.java
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -12,7 +12,8 @@ import org.junit.runners.Suite;
         MapResumeStorageTest.class,
         ObjectStreamStorageTest.class,
         XmlPathStorageTest.class,
-        JsonPathStorageTest.class
+        JsonPathStorageTest.class,
+        DataPathStorageTest.class
 })
 public class AllStorageTest {
 }
