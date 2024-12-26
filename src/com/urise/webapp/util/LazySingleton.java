6a184ccb44b26ff82f7b533eda7691141a0b841d
yanarsaeev <mackardyback@internet.ru>
2024-12-26 23:12:58 +0300

    add singleton

---
+++ src/com/urise/webapp/util/LazySingleton.java
@@ -0,0 +1,15 @@
+package com.urise.webapp.util;
+
+public class LazySingleton {
+    private static LazySingleton INSTANCE;
+
+    private LazySingleton() {}
+
+    private static class LazySingletonHolder {
+        private static final LazySingleton INSTANCE = new LazySingleton();
+    }
+
+    public static LazySingleton getInstance() {
+        return LazySingletonHolder.INSTANCE;
+    }
+}
