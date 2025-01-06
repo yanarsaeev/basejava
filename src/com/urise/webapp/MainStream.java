b20afe716886023ce0de67e73f0e8db402c47c56
yanarsaeev <mackardyback@internet.ru>
2025-01-06 21:21:05 +0300

    add minValue() and oddOrEven()

---
+++ src/com/urise/webapp/MainStream.java
@@ -0,0 +1,30 @@
+package com.urise.webapp;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStream {
+    public static void main(String[] args) {
+        System.out.println(minValue(new int[]{1, 2, 3, 3, 2, 3}));
+        System.out.println(minValue(new int[]{9, 8}));
+
+        List<Integer> integers = List.of(1, 2, 3, 4, 5);
+        System.out.println(oddOrEven(integers));
+    }
+
+    public static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (result, number) -> result * 10 + number);
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(Integer::intValue).sum();
+
+        return integers.stream()
+                .filter(num -> sum % 2 != 0 ? num % 2 == 0 : num % 2 != 0)
+                .collect(Collectors.toList());
+    }
+}
