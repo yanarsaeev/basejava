76a0242d257242203e985a4b2f164c3903e1b3f9
yanarsaeev <mackardyback@internet.ru>
2025-01-12 00:44:06 +0300

    updated exception constructor

--- src/com/urise/webapp/storage/PathStorage.java
+++ src/com/urise/webapp/storage/PathStorage.java
@@ -92,7 +92,7 @@ public class PathStorage extends AbstractStorage<Path> {
         try {
             return Files.list(directory);
         } catch (IOException e) {
-            throw new StorageException("getList error", null);
+            throw new StorageException("getList error", null, e);
         }
     }
 }
