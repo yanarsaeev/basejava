fd7cdfc9c447a95668d355f24a03856dd7d8be19
yanarsaeev <mackardyback@internet.ru>
2025-01-12 01:23:36 +0300

    add Config

---
+++ src/com/urise/webapp/Config.java
@@ -0,0 +1,43 @@
+package com.urise.webapp;
+
+import com.urise.webapp.storage.SqlStorage;
+import com.urise.webapp.storage.Storage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+    private static final File PROPS = new File("config\\resumes.properties");
+    private static final Config INSTANCE = new Config();
+
+    private final File storageDir;
+    private final Storage storage;
+
+    public static Config get() {
+        return INSTANCE;
+    }
+
+    private Config() {
+        try(InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
+            props.load(is);
+            storageDir = new File(props.getProperty("storage.dir"));
+            storage = new SqlStorage(props.getProperty("db.url"),
+                    props.getProperty("db.user"),
+                    props.getProperty("db.password"));
+        } catch (IOException e) {
+             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+        }
+    }
+
+    public File getStorageDir() {
+        return storageDir;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+}
