4d1a2e258c6546464fbec7493cf2e6956b71ff5f
yanarsaeev <mackardyback@internet.ru>
2025-01-18 23:48:18 +0300

    add transactionalExecute

--- src/com/urise/webapp/sql/SqlHelper.java
+++ src/com/urise/webapp/sql/SqlHelper.java
@@ -27,6 +27,22 @@ public class SqlHelper {
         execute(comm, PreparedStatement::execute);
     }
 
+    public <T> T transcationalExecute(SqlTransaction<T> executor) {
+        try (Connection conn = connectionFactory.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                T res = executor.execute(conn);
+                conn.commit();
+                return res;
+            } catch (SQLException e) {
+                conn.rollback();
+                throw getException(e);
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+
     private static StorageException getException(SQLException e) {
         if (e.getSQLState().equals("23505")) {
             return new ExistStorageException(null);
