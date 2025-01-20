12f0c867a62dcb3f2115eda36c7a8601a776c516
yanarsaeev <mackardyback@internet.ru>
2025-01-20 19:18:48 +0300

    add Comparator.comparing

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-    private static final Comparator<Resume> RESUME_COMPARATOR = (o1, o2) -> o1.getUuid().compareTo(o2.getUuid());
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
     @Override
     protected void insertElement(Resume r, int index) {
         index = -index - 1;
