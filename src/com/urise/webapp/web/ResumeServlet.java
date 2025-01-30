4434a0f03845e6a18e99d437b5f38cfc1f1f6364
yanarsaeev <mackardyback@internet.ru>
2025-01-30 23:57:03 +0300

    add init(), update doGet()

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,17 +1,59 @@
 package com.urise.webapp.web;
 
+import com.urise.webapp.Config;
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.Storage;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResumeServlet extends HttpServlet {
+    private final Storage storage = Config.get().getStorage();
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello resumes!" : "Hello " + name + '!');
+
+        PrintWriter writer = response.getWriter();
+
+        writer.write(
+                """
+                        <html>
+                        <head>
+                            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+                            <link rel="stylesheet" href="css/style.css">
+                            <title>Список резюме</title>
+                        </head>
+                        <body>
+                        <section>
+                        <table border="1" cellpadding="8" cellspacing="0">
+                            <tr>
+                                <th>FullName</th>
+                                <th>Uuid</th>
+                            </tr>
+                        """);
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write(
+                    "<tr>\n" +
+                            "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
+                            "     <td>" + resume.getUuid() + "</td>\n" +
+                            "</tr>\n");
+        }
+        writer.write("""
+                </table>
+                </section>
+                </body>
+                </html>
+                """);
     }
 
     @Override
