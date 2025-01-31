4c0ecb007e30b6ace763b080d5bf67a4c31a436a
yanarsaeev <mackardyback@internet.ru>
2025-02-01 01:24:44 +0300

    update doGet()

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -19,41 +19,8 @@ public class ResumeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-
-        PrintWriter writer = response.getWriter();
-
-        writer.write(
-                """
-                        <html>
-                        <head>
-                            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-                            <link rel="stylesheet" href="css/style.css">
-                            <title>Список резюме</title>
-                        </head>
-                        <body>
-                        <section>
-                        <table border="1" cellpadding="8" cellspacing="0">
-                            <tr>
-                                <th>FullName</th>
-                                <th>Uuid</th>
-                            </tr>
-                        """);
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write(
-                    "<tr>\n" +
-                            "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
-                            "     <td>" + resume.getUuid() + "</td>\n" +
-                            "</tr>\n");
-        }
-        writer.write("""
-                </table>
-                </section>
-                </body>
-                </html>
-                """);
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 
     @Override
